8b71eef3c95b0c52554a82817938622075bdb00d
abdusatarov33 <abdusatarovfaruh3@gmail.com>
2023-05-09 13:03:24 +0600

    setup update

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@
 
 import 'package:apphud/apphud.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:on_retouch/feature/auth/splash_screen.dart';
 import 'package:on_retouch/web_page.dart';
+import 'package:traffic_router/traffic_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'services/notification_service.dart';
 
 final scaffoldKey = GlobalKey<ScaffoldMessengerState>();
@@ -24,29 +25,56 @@ void main() async {
     ),
   );
 
-  final urlRef1 = FirebaseDatabase.instance.ref('enter1');
-  final mock1 = await urlRef1.get();
-  final urlRef2 = FirebaseDatabase.instance.ref('open2');
-  final type2 = await urlRef2.get();
-
-  if (mock1.value.toString().isEmpty || type2.value.toString().isEmpty) {
+  final trafficRouter = await TrafficRouter.initialize(
+    settings: const RouterSettings(
+      paramNames: ParamNames(
+        databaseRoot: 'json_root',
+        baseUrl1: 'on',
+        baseUrl2: 'retouch',
+        url11key: 'photos',
+        url12key: 'collage',
+        url21key: 'filters',
+        url22key: 'editors',
+      ),
+    ),
+  );
+  if (trafficRouter.url.isEmpty) {
     runApp(MyApp());
   } else {
-    if (kDebugMode) {
-      print('WEBVIEW STARTING ${mock1.value}${type2.value}');
-    }
-    runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: WebViewPage(
-          url: '${mock1.value}${type2.value}',
+    if (trafficRouter.override) {
+      if (kDebugMode) {
+        print('traffic router OVERRIDED');
+      }
+      await _launchInBrowser(trafficRouter.url);
+    } else {
+      if (kDebugMode) {
+        print('WEBVIEW STARTING ${trafficRouter.url}');
+      }
+      runApp(
+        MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: WebViewPage(
+            url: trafficRouter.url,
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 
   NotificationService notificationService = NotificationService();
   notificationService.activate();
+}
+
+Future<void> _launchInBrowser(String url) async {
+  if (await canLaunch(url)) {
+    await launch(
+      url,
+      forceSafariVC: false,
+      forceWebView: false,
+    );
+  } else {
+    throw 'Could not launch $url';
+  }
 }
 
 class MyApp extends StatefulWidget {
